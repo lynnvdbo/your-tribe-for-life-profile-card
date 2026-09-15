5ae4a6e9135945c6515fb40d3b1f71a5dfe41286
lynnvdbo <lynn.van.den.bosch@hva.nl>
2026-09-15 21:11:11 +0200

    #4 html toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="kaart-container">
+    <div class="buiten-rechthoek">       
+        <div class="binnen-rechthoek">
+            <div class="rechthoek-foto">
+                <img class="image" src="" width="300" height="360">
+            </div>
+                <section class="info">
+                    <h1>Lynn</h1>    
+                    <h2>Naam:</h2>
+                    <p>Lynn van den Bosch</p>
+                    <h2>Stad:</h2>
+                    <p>Utrecht</p>
+                    <h2>Bio:</h2>
+                    <p>Ik kijk er naar uit om mijn kennis te verbreden en mensen blij te maken met mijn werk.</p>
+                </section>
+        </div>
+    </div>
+
+    <div class="zwart-vlak"></div>
+</div>
+
+<nav>
+    <a href="https://github.com/lynnvdbo">
+        <h3>Github</h3>
+    </a>
+    <a href="#">
+        <h3>Portfolio</h3>
+    </a>
+    <a href="#">
+        <h3>Contact</h3>
+    </a>
+</nav>
+
