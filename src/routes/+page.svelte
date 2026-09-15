7636179c8f9fec1e7b9da73c813308019a3569e7
lynnvdbo <lynn.van.den.bosch@hva.nl>
2026-09-15 23:24:30 +0200

    #4 #8 <main> toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,26 +4,28 @@
     let person = data.person[32];
 </script>
 
-<div class="kaart-container">
-    <div class="buiten-rechthoek">       
-        <div class="binnen-rechthoek">
-            <div class="rechthoek-foto">
-                <img class="image" src="{ person.avatar }" alt="{ person.name }" width="300" height="360">
+<main>
+    <div class="kaart-container">
+        <div class="buiten-rechthoek">       
+            <div class="binnen-rechthoek">
+                <div class="rechthoek-foto">
+                    <img class="image" src="{ person.avatar }" alt="{ person.name }" width="300" height="360">
+                </div>
+                    <section class="info">
+                        <h1>{ person.name.split(' ')[0]}</h1>    
+                        <h2>Naam:</h2>
+                        <p>{ person.name }</p>
+                        <h2>Stad:</h2>
+                        <p>{ person.residency }</p>
+                        <h2>Bio:</h2>
+                        <p>{ person.bio }</p>
+                    </section>
             </div>
-                <section class="info">
-                    <h1>{ person.name.split(' ')[0]}</h1>    
-                    <h2>Naam:</h2>
-                    <p>{ person.name }</p>
-                    <h2>Stad:</h2>
-                    <p>{ person.residency }</p>
-                    <h2>Bio:</h2>
-                    <p>{ person.bio }</p>
-                </section>
         </div>
-    </div>
 
-    <div class="zwart-vlak"></div>
-</div>
+        <div class="zwart-vlak"></div>
+    </div>
+</main>
 
 <nav>
     <a href="https://github.com/lynnvdbo">
