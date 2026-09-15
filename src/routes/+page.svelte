23e59e4e6566737fedc06b32302e8f48c585c5d7
lynnvdbo <lynn.van.den.bosch@hva.nl>
2026-09-15 22:22:34 +0200

    #5 css toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,3 +37,89 @@
     </a>
 </nav>
 
+<style>
+
+    .kaart-container {
+        position: relative; /*alles wat absolute is, mag zich binnen deze container positioneren*/
+        width: 572px;
+        height: 728px;
+        margin: 5rem auto;
+    }
+    .buiten-rechthoek {
+        position: absolute;
+        inset: 0;
+        padding: 0.7rem;
+        width: 550px;
+        height: 700px;
+        background-color: var(--white);
+        z-index: 1;
+    }
+    .binnen-rechthoek {
+        display: grid;
+        align-items: flex-start;
+        width: 550px;
+        height: 700px;
+        background-color: var(--secondary-color);
+    }
+    .rechthoek-foto {
+        width: fit-content;
+        background-color: var(--white);
+        padding: 1rem 1rem 1.5rem 1rem ;
+        margin-top: 2rem;
+        margin-right: 10rem;
+        margin-left: 2rem;
+        img {
+            display: block;
+            width: 300px;
+            height: 360px;
+            object-fit: cover; /* zorgt er voor dat de foto niet vervormd is */
+        }
+    }
+    .info {
+        margin-left: 2rem;
+        margin-block: 2rem;
+    
+        h1 {
+            font-size: 11rem;
+            font-family: Sirivennela;
+            font-weight: 200;
+            text-transform: uppercase;
+            transform: rotate(-90deg);
+            position: absolute;
+            top: 40%;
+            left: 56%;
+            color: var(--black);
+        } 
+        h2 {
+            font-size: 1.1rem;
+            text-transform: uppercase;
+            font-family: boldonse, sans-serif;
+        }   
+
+        p {
+            margin-bottom: 0.9rem;
+        }
+    }
+    nav {
+        display: flex;
+        gap: 1.5rem;
+        position: fixed;
+        top: 2rem;
+        left: 3rem;
+        z-index: 1000; 
+        font-family: boldonse, sans-serif;
+        h3 {
+            font-size: 1rem;
+        }
+        a {
+            text-decoration: none;
+            color: var(--white);
+            text-transform: uppercase;
+            &:hover {
+                text-decoration: underline;
+                color: var(--secondary-color);
+            }
+        }
+    }
+    } */
+</style>
